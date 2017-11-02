3284beb3778a7caedbdd955b9ca15853aa797095
Doug Joseph <doug.joseph@hotmail.com>
2017-11-02 15:51:25 +0000

    TEST

---
+++ SmallGame2/Assets/Doug_WIP/Scripts/Candle_Order.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Candle_Order : MonoBehaviour {
+
+    
+
+
+    private void Start()
+    {
+        
+
+    }
+
+
+    private void Update()
+    {
+        if (Raycast_for_Candle.bl_IsHit)
+        {
+
+        }
+
+    }
+
+}
---
+++ SmallGame2/Assets/Doug_WIP/Scripts/Raycast_for_Candle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raycast_for_Candle : MonoBehaviour {
+
+    public Collider coll;
+    public static bool bl_IsHit;
+
+
+    void Start()
+    {
+        coll = GetComponent<Collider>();
+    }
+
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (coll.Raycast(ray, out hit, 100.0F))
+            {
+                bl_IsHit = true;
+            }
+        }
+    }
+}
