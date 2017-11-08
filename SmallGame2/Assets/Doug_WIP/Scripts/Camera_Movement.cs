50567159a4f0d482704efe509138c4804daadda0
Doug Joseph <doug.joseph@hotmail.com>
2017-11-08 20:36:49 +0000

    Added and Changed Camera Script
    Camera Script now rotates around it's central point instead of moving. Will need editing for adding UI Button compatibility

---
+++ SmallGame2/Assets/Doug_WIP/Scripts/Camera_Movement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Camera_Movement : MonoBehaviour {
+
+    bool bl_IsDown;
+
+	// Use this for initialization
+	void Start () {
+
+        bl_IsDown = false;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if ((Input.GetKeyDown(KeyCode.LeftArrow)) && (bl_IsDown == false))
+        {
+            transform.Rotate(new Vector2(0, 90));
+        }
+        if ((Input.GetKeyDown(KeyCode.RightArrow)) && (bl_IsDown == false))
+        {
+            transform.Rotate(new Vector2(0, -90));
+        }
+        if ((Input.GetKeyDown(KeyCode.DownArrow)) && (bl_IsDown == false))
+        {
+            transform.Rotate(new Vector2(90, 0));
+            bl_IsDown = true;
+        }
+        if ((Input.GetKeyDown(KeyCode.UpArrow)) && (bl_IsDown == true))
+        {
+            transform.Rotate(new Vector2(-90, 0));
+            bl_IsDown = false;
+        }
+
+    }
+}
